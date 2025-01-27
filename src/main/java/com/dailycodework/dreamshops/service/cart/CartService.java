e940f6b219a72deddbc95da09d37022fc7e339c9
Richard Thiam <guayazan2003@gmail.com>
2025-01-26 19:10:53 -0500

    Servicios de entidades y funciones en entidad Cart

---
+++ src/main/java/com/dailycodework/dreamshops/service/cart/CartService.java
@@ -0,0 +1,54 @@
+package com.dailycodework.dreamshops.service.cart;
+
+import java.math.BigDecimal;
+
+import org.springframework.stereotype.Service;
+
+import com.dailycodework.dreamshops.exceptions.ResourceNotFoundException;
+import com.dailycodework.dreamshops.model.Cart;
+import com.dailycodework.dreamshops.model.CartItem;
+import com.dailycodework.dreamshops.repository.CartItemRepository;
+import com.dailycodework.dreamshops.repository.CartRepository;
+
+import lombok.RequiredArgsConstructor;
+
+
+@Service
+@RequiredArgsConstructor
+
+public class CartService implements ICartService {
+
+    private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
+
+    @Override
+    public Cart getCart(Long id) {
+        Cart cart = cartRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Cart no encontrado"));
+        BigDecimal totalAmount = cart.getTotalAmount();
+        cart.setTotalAmount(totalAmount);
+        return cartRepository.save(cart);
+    }
+
+    @Override
+    public void clearCart(Long id) {
+        Cart cart = getCart(id);
+        //Elimina todos los elementos del carro que esten asociados al id en la BD.
+        cartItemRepository.deleteAllByCartId(id);
+        //Elimina todos los elementos del carro en memoria.
+        cart.getItems().clear();
+        cartRepository.deleteById(id);
+    }
+
+    @Override
+    public BigDecimal getTotalPrice(Long id) {
+        Cart cart = getCart(id);
+        return cart.getTotalAmount();
+        // Con esto se hace lo mismo que el getTotalAmount.
+        // return cart.getItems().stream()
+        //     .map(CartItem :: getTotalPrice)
+        //     // Al llamar getTotalPrice de la variable totalPrice en CartItem, este en el modelo llama a setTotalPrice.
+        //     .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+}
