4bcec5e5fdf7071b21ba71bf8a9a18a49a9d1df4
Richard Thiam <guayazan2003@gmail.com>
2025-01-29 22:14:08 -0500

    Entidad Order

--- src/main/java/com/dailycodework/dreamshops/model/Order.java
+++ src/main/java/com/dailycodework/dreamshops/model/Order.java
@@ -8,7 +8,6 @@ import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -17,14 +16,19 @@ import java.util.Set;
 @NoArgsConstructor
 @Entity
 @Table(name = "orders")
+
 public class Order {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderId;
+
     private LocalDate orderDate;
     private BigDecimal totalAmount;
+
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;
+
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<OrderItem> orderItems = new HashSet<>();
 
