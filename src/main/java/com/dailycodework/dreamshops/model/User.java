665f5a33c902e0413bb83a98d523b75a07f2a21f
Richard Thiam <guayazan2003@gmail.com>
2025-02-10 23:40:31 -0500

    Creando tabla usuario_role

--- src/main/java/com/dailycodework/dreamshops/model/User.java
+++ src/main/java/com/dailycodework/dreamshops/model/User.java
@@ -12,6 +12,8 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
@@ -47,8 +49,12 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Order> orders;
 
+    //Esto crea una tabla aparte para la relacion role-usuario
     @ManyToMany(fetch = FetchType.EAGER, cascade = 
         {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
     private Collection<Role> roles = new HashSet<>();
     
 }
