7206d6c3028ef9c665de25ac2433f6a99a96f378
Richard Thiam <guayazan2003@gmail.com>
2025-02-13 12:02:07 -0500

    Manejo del token

--- src/main/java/com/dailycodework/dreamshops/security/jwt/JwtUtils.java
+++ src/main/java/com/dailycodework/dreamshops/security/jwt/JwtUtils.java
@@ -4,6 +4,7 @@ import java.security.Key;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
@@ -24,12 +25,16 @@ import io.jsonwebtoken.security.SignatureException;
 
 public class JwtUtils {
     
+    @Value("${auth.token.jwtSecret}")
     private String jwtSecret;
+
+    @Value("${auth.token.expirationsInMils}")
     private int expirationTime;
 
     public String generateTokenForUser(Authentication authentication) {
         ShopUserDetails userPrincipal = (ShopUserDetails) authentication.getPrincipal();
 
+        //Extraemos las autorizaciones con los roles que tiene user asignado
         List<String> roles = userPrincipal.getAuthorities()
             .stream()
             .map(GrantedAuthority::getAuthority).toList();
@@ -37,6 +42,8 @@ public class JwtUtils {
         //Información que queremos codificar dentro del token del usuario
         //Así, lo que se quiera codificar para el usuario se pasa como un .claim
         //Y se tendría que incluir en el ShopUserDetails
+        // Aquí se pasa como claim los roles ya que se extraen con la autenticación
+        // Se pasa una key con la función Key
         return Jwts.builder()
             .setSubject(userPrincipal.getEmail())
             .claim("id", userPrincipal.getId())
@@ -50,6 +57,7 @@ public class JwtUtils {
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
     }
 
+    //Extrae el username del token 
     public String getUsernameFromToken(String token){
         return Jwts.parserBuilder()
             .setSigningKey(key())
@@ -59,7 +67,6 @@ public class JwtUtils {
     }
 
     public boolean validateToken(String token){
-        
         try {
             Jwts.parserBuilder()
                 .setSigningKey(key())
